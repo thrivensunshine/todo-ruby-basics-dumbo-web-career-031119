82ed189007c0263b61638d4c5f38e8c6b2c7f56d
Danielle <danib.sgh@gmail.com>
2019-02-20 19:42:23 -0500

    done booyah

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = value
+  value
 end
 
-def argue
-  
-end
-
-def greeting
-
-end
-
-def return_a_value
+def argue(arg)
+  phrase = "I'm right and you are wrong!"
+  phrase
 
 end
 
-def last_evaluated_value
-
+def greeting(greet,person)
+phrase = "#{greet} #{person}!"
+phrase
 end
 
-def pizza_party
+def return_a_value(val = "Nice")
+  val
+end
 
+def last_evaluated_value(saying = "expert")
+  saying
+end
+
+def pizza_party(type = "cheese")
+  type
 end
